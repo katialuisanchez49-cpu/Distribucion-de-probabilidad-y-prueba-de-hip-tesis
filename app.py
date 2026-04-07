3845d4e89309178b2bbb6939a472f273bf77dd19
ayytning <katialuisanchez.49@gmail.com>
2026-04-07 03:45:48 -0600

    visualizacion de graficas y bloxplot, y un analisis automatico, falta modificar el formato

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import streamlit as st
 from modulos.carga_datos import modulo_carga_datos
+from modulos.visualizacion import modulo_visualizacion
 
-st.set_page_config(page_title="Análisis Estadístico", 
+st.set_page_config(page_title="Análisis Estadístico",
                    page_icon="📊", layout="wide")
 
 st.title("📊 Aplicación de Probabilidad y Estadística")
 st.markdown("---")
 
-modulo_carga_datos()
+st.sidebar.title("🧭 Navegación")
+modulo = st.sidebar.radio("Selecciona un módulo", [
+    "📂 Carga de Datos",
+    "📊 Visualización"
+])
+
+if modulo == "📂 Carga de Datos":
+    modulo_carga_datos()
+elif modulo == "📊 Visualización":
+    modulo_visualizacion()
